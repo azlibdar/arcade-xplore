b8967ec59b614165814b4f17abcb6512f8ba3b05
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-04 21:51:38 +0530

    fix: null critic score

--- src/components/ui/CriticScore.tsx
+++ src/components/ui/CriticScore.tsx
@@ -1,8 +1,16 @@
 interface Props {
-  score: number;
+  score: number | null;
 }
 
 const CriticScore = ({ score }: Props) => {
+  if (score === null) {
+    return (
+      <span title="Critic score" className="text-zinc-400 py-1 h-min px-2 font-medium rounded-md text-sm bg-zinc-700">
+        N/A
+      </span>
+    );
+  }
+
   const colorClass = score >= 75 ? "text-green-400" : score >= 50 ? "text-amber-400" : "text-zinc-400";
   return (
     <span title="Critic score" className={`${colorClass} py-1 h-min px-2 font-medium rounded-md text-sm bg-zinc-700`}>
