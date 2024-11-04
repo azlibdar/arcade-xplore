00db1d1e5e597aaf1adf33ba9530c52f8608ab7e
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-04 22:29:08 +0530

    fix: include all platform icons

--- src/components/ui/GameCard.tsx
+++ src/components/ui/GameCard.tsx
@@ -8,7 +8,7 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <div className="bg-zinc-800 rounded-xl p-4 cursor-pointer">
+    <div className="bg-zinc-800 rounded-xl p-4 cursor-pointer h-min">
       <img
         src={game.background_image}
         loading="lazy"
@@ -16,11 +16,11 @@ const GameCard = ({ game }: GameCardProps) => {
         className="w-full aspect-video transition object-cover rounded-lg bg-zinc-900"
       />
       <div className="py-4 flex flex-col">
-        <div className="flex justify-between gap-4">
+        <h3 className="text-lg font-semibold text-zinc-200">{game.name}</h3>
+        <div className="flex justify-between gap-4 pt-3">
           <PlatformIcons platforms={game.parent_platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </div>
-        <h3 className="text-lg font-semibold text-zinc-200 mt-3">{game.name}</h3>
       </div>
     </div>
   );
--- src/components/ui/PlatformIcons.tsx
+++ src/components/ui/PlatformIcons.tsx
@@ -8,6 +8,11 @@ import logoNintendo from "../../assets/logo/logo-nintendo.svg";
 import logoIos from "../../assets/logo/logo-ios.svg";
 import logoAndroid from "../../assets/logo/logo-android.svg";
 import logoWeb from "../../assets/logo/logo-web.svg";
+import logoAtari from "../../assets/logo/logo-atari.svg";
+import logoSega from "../../assets/logo/logo-sega.svg";
+import logoCommodore from "../../assets/logo/logo-commodore.svg";
+import threedo from "../../assets/logo/logo-3do.svg";
+import neogeo from "../../assets/logo/logo-neo-geo.svg";
 
 interface Props {
   platforms: Platform[];
@@ -24,6 +29,11 @@ const PlatformIcons = ({ platforms }: Props) => {
     ios: logoIos,
     android: logoAndroid,
     web: logoWeb,
+    atari: logoAtari,
+    "commodore-amiga": logoCommodore,
+    sega: logoSega,
+    "3do": threedo,
+    "neo-geo": neogeo,
   };
 
   return (
