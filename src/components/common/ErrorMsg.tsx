fdd9604968e890ddde50d473c9bf2d66892a12c3
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-10 15:59:26 +0530

    fix: error icon

--- src/components/common/ErrorMsg.tsx
+++ src/components/common/ErrorMsg.tsx
@@ -1,4 +1,4 @@
-import { AlertIcon } from "@primer/octicons-react";
+import { Alert02Icon } from "hugeicons-react";
 
 interface Props {
   errorMsg: string;
@@ -9,7 +9,7 @@ interface Props {
 const ErrorMsg = ({ errorMsg, className, IconSize = 24 }: Props) => {
   return (
     <div className={`w-full flex flex-col rounded-lg items-center justify-center text-center leading-normal ${className}`}>
-      <AlertIcon size={IconSize} className="text-red-400" />
+      <Alert02Icon size={IconSize} className="text-red-400" />
       <p className="max-w-md">{errorMsg}</p>
     </div>
   );
