6aa23c78ae30d49bbfc1876007c7486457d59ffa
Azlan Ibrahim <azlibdar@gmail.com>
2024-11-10 19:23:27 +0530

    add: clear search button

--- src/components/ui/MainSearch.tsx
+++ src/components/ui/MainSearch.tsx
@@ -1,4 +1,4 @@
-import { Search01Icon } from "hugeicons-react";
+import { CancelCircleIcon, Search01Icon } from "hugeicons-react";
 import { useState } from "react";
 
 interface Props {
@@ -6,14 +6,7 @@ interface Props {
 }
 
 const MainSearch = ({ onSearchQuery }: Props) => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-
-  const handleSearchChange = (value: string) => {
-    setSearchQuery(value);
-    if (value === "") {
-      onSearchQuery(null);
-    }
-  };
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSubmission = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,14 +25,29 @@ const MainSearch = ({ onSearchQuery }: Props) => {
         id="search"
         name="search"
         value={searchQuery}
-        onChange={(e) => handleSearchChange(e.target.value)}
+        onChange={(e) => setSearchQuery(e.target.value)}
         enterKeyHint="go"
         placeholder="Press enter to search.."
-        className="w-full p-3 pl-8 rounded-lg sm:text-sm transition bg-zinc-800 text-zinc-200 placeholder:text-zinc-400 border-0 outline-none"
+        className="w-full p-3 pl-8 pr-9 rounded-lg sm:text-sm transition bg-zinc-800 text-zinc-200 placeholder:text-zinc-400 border-0 outline-none"
       />
-      <div className="absolute pointer-events-none inset-0 h-full w-min px-3 flex justify-center items-center text-zinc-400">
+      <div className="absolute pointer-events-none left-0 top-0 h-full w-min px-3 flex justify-center items-center text-zinc-400">
         <Search01Icon size={15} />
       </div>
+      {searchQuery.trim() && (
+        <div className="absolute top-0 right-0 h-full w-min flex justify-center items-center">
+          <button
+            title="Clear search"
+            onClick={() => {
+              setSearchQuery("");
+              onSearchQuery(null);
+            }}
+            type="button"
+            className="p-2 pr-3 rounded-md flex justify-center items-center text-zinc-400 transition hover:text-rose-400 active:translate-y-[1px]"
+          >
+            <CancelCircleIcon size={18} />
+          </button>
+        </div>
+      )}
     </form>
   );
 };
